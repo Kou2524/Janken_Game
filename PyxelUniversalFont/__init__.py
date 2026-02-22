f2fba98762ea62947900f6d8e0d60cd6c10ce340
Kou2524 <yasumaruyamu@gmail.com>
2026-02-23 02:19:06 +0900

    Refactor imports in __init__.py for clarity

--- PyxelUniversalFont/__init__.py
+++ PyxelUniversalFont/__init__.py
@@ -1,3 +1,9 @@
-from .src.root import *
-from .src.utils import *
-from . import *
+from .src.root import Writer
+# もしutilsの中身も使うなら必要なものだけ追加でimport
+# from .src.utils import 〇〇
+
+__all__ = ["Writer"]
+
+#from .src.root import *
+#from .src.utils import *
+#from . import *
